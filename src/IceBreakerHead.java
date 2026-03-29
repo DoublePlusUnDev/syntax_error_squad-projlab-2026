a3fc3b0870e058a2a6993302566a2f5bd84b3f70
DoublePlusUnDev <andri.takacs@gmail.com>
2026-03-29 23:31:26 +0200

    icebreakerhead text fix

--- src/IceBreakerHead.java
+++ src/IceBreakerHead.java
@@ -6,11 +6,11 @@ public class IceBreakerHead extends PlowHead {
 
     @Override
     public void clean(Lane lane) {
-        TestUtil.enterFunction("DragonHead:clean(lane)");
+        TestUtil.enterFunction("IceBreakerHead:clean(lane)");
 
         lane.breakIce();
         
-        TestUtil.exitFunction("ice broken");
+        TestUtil.exitFunction("ice breaker head used");
     }
     
 }
