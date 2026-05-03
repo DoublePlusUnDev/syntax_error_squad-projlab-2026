d9a9843174c7540616f50677fba8b3e9c529fbab
DoublePlusUnDev <andri.takacs@gmail.com>
2026-05-03 19:59:03 +0200

    Welcome text

--- src/Prototype.java
+++ src/Prototype.java
@@ -6,6 +6,9 @@ public class Prototype {
         
         Scanner scanner = new Scanner(System.in);
         
+        System.out.println("Welcome to the Traffic Simulator Prototype!");
+        System.out.println("Made by Syntax Error Squad for BME Projlab");
+        System.out.println("Enter commands to interact with the game. Type 'help' for a list of commands.");
         while (true) {
             System.out.print("> ");
             String input = scanner.nextLine();
